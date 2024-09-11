6b4208e8c93c11ee32b15b32770c3e255e40d32d
Zeeshan Asghar <mzeeshanff10@gmail.com>
2024-09-11 15:15:58 +0500

    Deploy latest code

--- Client/supplier-frontend/src/main.jsx
+++ Client/supplier-frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-import ThemeProvider from "./context/ThemeContext ";
+import ThemeProvider from "./context/ThemeContext";
 
 // Render the app with routing support
 createRoot(document.getElementById("root")).render(
