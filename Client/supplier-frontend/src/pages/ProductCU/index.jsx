c3be85b3ec1b769aecbcd8bb066f62faf84693f5
Zeeshan Asghar <mzeeshanff10@gmail.com>
2024-10-21 14:43:55 +0500

    Deploy latest code

--- Client/supplier-frontend/src/pages/ProductCU/index.jsx
+++ Client/supplier-frontend/src/pages/ProductCU/index.jsx
@@ -2,33 +2,36 @@ import styles from "./index.module.css";
 import ShowcaseGrid from ".././../components/common/ShowcaseV2/ShowcaseGrid";
 import StatusSidebar from "./components/StatusSidebar";
 import ProductForm from "./components/ProductForm";
+import ProductFormProvider from "./contexts/ProductForm";
 
 function ProductCU() {
   return (
-    <main className={styles.productCU}>
-      <ShowcaseGrid
-        customClass={styles.productDetails}
-        showcaseHeaderProps={{
-          customClass: styles.showcaseHeader,
-          showFeatureMsg: false,
-          showFilterTabs: false,
+    <ProductFormProvider>
+      <main className={styles.showcaseGrid}>
+        <ShowcaseGrid
+          customClass={styles.productDetails}
+          showcaseHeaderProps={{
+            customClass: styles.showcaseHeader,
+            showFeatureMsg: false,
+            showFilterTabs: false,
 
-          headerWrapperProps: {
-            showcaseName: "Add Product",
-            showFilterButtons: false,
-          },
-        }}
-        showcaseContentProps={{
-          customClass: styles.showcaseContent,
-          wrapContent: true,
-          StatusSidebar: StatusSidebar,
+            headerWrapperProps: {
+              showcaseName: "Add Product",
+              showFilterButtons: false,
+            },
+          }}
+          showcaseContentProps={{
+            customClass: styles.showcaseContent,
+            wrapContent: true,
+            StatusSidebar: StatusSidebar,
 
-          showcaseWrapperProps: {
-            ShowcaseContainer: ProductForm,
-          },
-        }}
-      />
-    </main>
+            showcaseWrapperProps: {
+              ShowcaseContainer: ProductForm,
+            },
+          }}
+        />
+      </main>
+    </ProductFormProvider>
   );
 }
 
