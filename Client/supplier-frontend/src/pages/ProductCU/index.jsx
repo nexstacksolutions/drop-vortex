020ca3d1a9c8fc25163a39044fff324ec7a89227
Zeeshan Asghar <mzeeshanff10@gmail.com>
2024-10-21 15:20:34 +0500

    Deploy latest code

--- Client/supplier-frontend/src/pages/ProductCU/index.jsx
+++ Client/supplier-frontend/src/pages/ProductCU/index.jsx
@@ -1,35 +1,33 @@
 import styles from "./index.module.css";
-import ShowcaseGrid from ".././../components/common/ShowcaseV2/ShowcaseGrid";
 import StatusSidebar from "./components/StatusSidebar";
 import ProductForm from "./components/ProductForm";
 import ProductFormProvider from "./contexts/ProductForm";
+import ShowcaseHeader from "../../components/common/ShowcaseV2/ShowcaseHeader";
+import classNames from "classnames";
+
+function ShowcaseContent() {
+  return (
+    <section className={classNames(styles.showcaseContent, "flex")}>
+      <ProductForm />
+      <StatusSidebar />
+    </section>
+  );
+}
 
 function ProductCU() {
   return (
     <ProductFormProvider>
       <main className={styles.showcaseGrid}>
-        <ShowcaseGrid
-          customClass={styles.productDetails}
-          showcaseHeaderProps={{
-            customClass: styles.showcaseHeader,
-            showFeatureMsg: false,
-            showFilterTabs: false,
-
-            headerWrapperProps: {
-              showcaseName: "Add Product",
-              showFilterButtons: false,
-            },
-          }}
-          showcaseContentProps={{
-            customClass: styles.showcaseContent,
-            wrapContent: true,
-            StatusSidebar: StatusSidebar,
-
-            showcaseWrapperProps: {
-              ShowcaseContainer: ProductForm,
-            },
+        <ShowcaseHeader
+          customClass={styles.showcaseHeader}
+          showFeatureMsg={false}
+          showFilterTabs={false}
+          headerWrapperProps={{
+            showcaseName: "Add Product",
+            showFilterButtons: false,
           }}
         />
+        <ShowcaseContent />
       </main>
     </ProductFormProvider>
   );
